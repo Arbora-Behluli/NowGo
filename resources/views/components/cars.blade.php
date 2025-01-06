fd928ee068e9ebd7a9e14d9bf9113fd4b6e4cae8
Arbora-Behluli <arbora.behluli2@gmail.com>
2025-01-07 00:48:25 +0100

    added swiper, FAQ, safety part and the swiper for feedbacks

--- resources/views/components/cars.blade.php
+++ resources/views/components/cars.blade.php
@@ -1,43 +1,43 @@
-
 <div id="cars" class="text-center pt-10 mt-6 sm:mt-8 scroll-mt-10">
 
-    <h1 class="mobile-app-text text-4xl w-full sm:w-1/2 px-4 sm:px-0 m-auto title font-extrabold text-[#290e07]">Features you will have</h1>
+    <h1 class="mobile-app-text text-4xl w-full sm:w-1/2 px-4 sm:px-0 m-auto title font-extrabold text-[#290e07]">Our Main Features</h1>
     <p class="text-base sm:text-lg mt-4 text-gray-700 w-full sm:w-1/2 m-auto title font-[500] mb-16">
-        Discover a range of features designed to enhance your travel experience. From rating trips and making secure online payments to staying connected through seamless communication, our platform ensures comfort, safety, and convenience for every journey.
+        NowGo is committed to making your travel smoother and more enjoyable, every step of the way.
+        We offer a range of innovative features crafted to elevate your oredering experience.
     </p>
-    <div id="box-container" class="flex flex-col sm:flex-row sm:space-x-4 justify-center gap-8 max-w-screen-xl m-auto mb-10 md:px-4">
+    <div id="box-container" class="flex flex-col sm:flex-row sm:space-x-4 justify-center gap-8 max-w-screen-xl m-auto mb-10 md:px-4 ">
 
 
-        <div id="box" class="myBox mt-6 w-60 flex flex-col items-center justify-center rounded-2xl relative mx-auto" style="margin-left: 0;">
+        <div id="box" class="myBox mt-6 w-60 flex flex-col items-center justify-center rounded-2xl relative mx-auto" style="background-color: #ffffa9; margin-left: 0;">
 
 
-            <img src="{{ asset('storage/landing/rate.png') }}" alt="Sample Image" class="w-32 h-32 mb-4" />
+            <img src="{{ asset('storage/landing/d.png') }}" alt="Sample Image" class="w-32 h-32 mb-4" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7m-9 2v10m-4-6h8" />
             <h2 class="text-2xl text-[#290e07]  mt-2 title font-[700]">
-                <a href="about:blank" target="_blank">Rate the trip</a>
+                <a href="about:blank" target="_blank">Fast Delivery</a>
             </h2>
-            <p class="text-center text-gray-700 mt-2 px-2">Provide feedback on your travel experience by rating your trip. Help others make informed decisions and let drivers know about their performance.</p>
+            <p class="text-center text-gray-700 mt-2 px-2">We understand the importance of timely deliveries. That's why we focus on providing fast delivery services to ensure your packages reach their destination quickly and securely.</p>
         </div>
-        <div id="box" class="myBox mt-6 w-60 flex flex-col items-center justify-center rounded-2xl relative mx-auto" style="margin-left: 0;">
+        <div id="box" class="myBox mt-6 w-60 flex flex-col items-center justify-center rounded-2xl relative mx-auto " style="margin-left: 0;">
 
 
-            <img src="{{ asset('storage/landing/op.png') }}" alt="Sample Image" class="w-32 h-32 mb-4" />
+            <img src="{{ asset('storage/landing/trade.png') }}" alt="Sample Image" class="w-32 h-32 mb-4" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7m-9 2v10m-4-6h8" />
             <h2 class="text-2xl text-[#290e07]  mt-2 title font-[700]">
-                <a href="about:blank" target="_blank">Online Payment</a>
+                <a href="about:blank" target="_blank">Affordable Prices</a>
             </h2>
-            <p class="text-center text-gray-700 mt-2 px-2">Simplify your booking process with seamless online payment options. Quickly select your destination, pay securely, and enjoy your trip.</p>
+            <p class="text-center text-gray-700 mt-2 px-2">We believe that quality service shouldn’t come at a high cost. Our affordable prices ensure that you get reliable and efficient delivery solutions without exceeding your budget. </p>
         </div>
 
         <div id="box" class="myBox mt-6 w-60 flex flex-col items-center justify-center rounded-2xl relative mx-auto" style="margin-left: 0;">
 
 
-            <img src="{{ asset('storage/landing/chat.png') }}" alt="Sample Image" class="w-32 h-32 mb-4"/>
+            <img src="{{ asset('storage/landing/route.png') }}" alt="Sample Image" class="w-32 h-32 mb-4"/>
             <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7m-9 2v10m-4-6h8" />
             <h2 class="text-2xl text-[#290e07] font-bold mt-2 title font-[700]">
                 <a class="text-lg font-bold text-black mt-2">
-                    <a href="about:blank" target="_blank">Chat with your nowgo</a>
+                    <a href="about:blank" target="_blank">Tracking and Transparency</a>
             </h2>
-            <p class="text-center text-gray-700 mt-2 px-2">Stay connected with your passenger or driver using the in-app chat feature. Enhance coordination and enjoy a stress-free journey with uninterrupted communication.</p>
+            <p class="text-center text-gray-700 mt-2 px-2">we prioritize tracking and transparency to give you peace of mind throughout the delivery process. Our real-time tracking system allows you to monitor your package every step of the way, ensuring that you stay informed and in control.</p>
         </div>
     </div>
