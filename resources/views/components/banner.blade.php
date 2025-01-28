69fbe9100aa35b90a2491d21000d019b276775e6
Arbora-Behluli <arbora.behluli2@gmail.com>
2025-01-28 14:04:20 +0100

    new banner

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,13 +1,13 @@
-<div class="w-full flex flex-col justify-center items-center text-center bg-cover bg-center py-12 relative">
+    <div class="w-full flex flex-col justify-center items-center text-center bg-cover bg-center py-12 relative">
 
-     <img src="{{ asset('storage/landing/b.jpg') }}" alt="Background Image" class="hidden md:block absolute inset-0 w-full h-full object-contain">  <img src="{{ asset('storage/landing/b.jpg') }}" alt="Background Image" class="hidden md:block absolute inset-0 w-full h-full object-contain">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 absolute md:relative">
+        <img src="{{ asset('storage/landing/b.jpg') }}" alt="Background Image" class="hidden md:block absolute inset-0 w-full h-full object-contain">  <img src="{{ asset('storage/landing/b.jpg') }}" alt="Background Image" class="hidden md:block absolute inset-0 w-full h-full object-contain">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 absolute md:relative">
 
-        <div class="flex justify-center items-center w-full">
-            <div class="relative h-[320px] md:h-[530px] lg:h-[785px] w-full flex items-center justify-center text-white mx-auto">
+            <div class="flex justify-center items-center w-full">
+                <div class="relative h-[320px] md:h-[530px] lg:h-[785px] w-full flex items-center justify-center text-white mx-auto">
 
-                <div class="absolute w-full sm:w-[150%] md:w-[150%] h-48 md:h-60 mt-16 bg-black opacity-50 left-1/2 transform -translate-x-1/2"></div>
+                    <div class="absolute w-full sm:w-[150%] md:w-[150%] h-48 md:h-60 mt-16 bg-black opacity-50 left-1/2 transform -translate-x-1/2"></div>
+                </div>
             </div>
         </div>
     </div>
-</div>
